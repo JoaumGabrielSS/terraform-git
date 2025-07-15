9e178c39209fcfdeec22b0b12a41e20b1eea7eff
JoaumGabrielSS <bielsilva360@gmail.com>
2025-07-15 13:59:33 -0300

    Sobe toda infra na AWS

---
+++ Modules/Bucket/bucket.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "bucket_1" {
+  bucket = "gabriel-terraform-remote-state777-1"
+}
+
+resource "aws_s3_bucket" "bucket_2" {
+  bucket = "gabriel-terraform-remote-state777-2"
+}
+
+resource "aws_s3_bucket" "bucket_3" {
+  bucket = "gabriel-terraform-remote-state777-3"
+}
